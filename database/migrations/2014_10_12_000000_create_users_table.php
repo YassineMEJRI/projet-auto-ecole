4628d173558c094db1b3dd0f89b91cd06e6a778b
Yassine Mejri <yassinemejri5@gmail.com>
2021-08-20 23:30:27 +0100

    added droit to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('droit')->default(0);
             $table->integer('tel');
             $table->rememberToken();
             $table->timestamps();
