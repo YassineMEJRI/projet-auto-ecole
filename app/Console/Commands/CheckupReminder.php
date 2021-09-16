d7cd565cdc463fcb64764d7a9fe3a43d89ce0e20
mariem <mariemzarai84@gmail.com>
2021-09-17 00:16:47 +0100

    checkup Command

---
+++ app/Console/Commands/CheckupReminder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
+
+
+class CheckupReminder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'Users:checkupVehicule';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'notify admin  of the vehicules\'s checkup date ';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+    log::info('time is ' . Carbon::now()) ;
+    }
+
+}
