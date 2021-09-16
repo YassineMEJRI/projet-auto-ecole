18f8ab619a63c0c805598cbcc913e2f2a7462828
Yassine Mejri <yassinemejri5@gmail.com>
2021-09-16 22:46:46 +0100

    min hours in offers

--- resources/views/offers.blade.php
+++ resources/views/offers.blade.php
@@ -2,10 +2,10 @@
 @section('content')
     <form>
         <label for="heuresCode" class="form-label">Nombre d'heures de code: 7dt/hr</label>
-        <input type="range" class="form-range" min="0" max="20" value="5" step="1" name="heuresCode" id="heuresCode" onchange="somme()">
+        <input type="range" class="form-range" min="5" max="20" value="5" step="1" name="heuresCode" id="heuresCode" onchange="somme()">
         <div id="nbHrCode">5 heures</div> <div id="sommeCode">35 dt</div>
         <label for="heuresConduite" class="form-label">Nombre d'heures de conduite: 20dt/hr</label>
-        <input type="range" class="form-range" min="0" max="50" value="10" step="1" name="heuresConduite" id="heuresConduite" onchange="somme()">
+        <input type="range" class="form-range" min="10" max="50" value="10" step="1" name="heuresConduite" id="heuresConduite" onchange="somme()">
         <div id="nbHrConduite">10 heures</div> <div id="sommeConduite">200 dt</div>
         <div><h3 id="somme" class="text-success fw-bold">235 dt</h3></div>
         <a class="btn btn-primary" href="/paiement">Passer au paiement</a>
